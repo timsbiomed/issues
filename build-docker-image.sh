6dd5780181ef7b281b56f16af5c5ffa9b144a4d1
Chris Roeder <chris.roeder@cuanschutz.edu>
2022-10-27 16:07:56 -0600

    added docker equivalent of clean command, used my own tags to be able to push to my docker repo(\?), not hapi-fhir's, can formalize to a group one later

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-docker build -t hapi-fhir/hapi-fhir-jpaserver-starter .
+
+docker rm $(docker ps --filter=status=exited --filter=status=created -q)
+docker rmi $(docker images -a -q)
+docker builder prune
+
+#docker build -t hapi-fhir/hapi-fhir-jpaserver-starter .
+docker build -t chrisroederucdenver/hapi-fhir-jpaserver-starter:v6.2-SS .
 
